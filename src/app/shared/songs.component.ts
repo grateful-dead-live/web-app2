76032149372711c21742e3ba448ec0835e60229f
muddymudskipper <thomas.wilmering@gmail.com>
2019-05-25 14:38:09 +0100

    show view dev

--- src/app/shared/songs.component.ts
+++ src/app/shared/songs.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { DeadEventInfo } from '../services/types';
+import { DeadApiService } from '../services/dead-api.service';
 
 @Component({
   selector: 'gd-songs',
@@ -8,4 +9,14 @@ import { DeadEventInfo } from '../services/types';
 export class SongsComponent {
   @Input() events: DeadEventInfo[];
   @Input() audio: [];
+
+  selectedRec: string ;
+
+ 
+  selectChangeHandler (event: any) {
+    //update the ui
+    this.selectedRec = event.target.value;
+  }
+
+
 }
