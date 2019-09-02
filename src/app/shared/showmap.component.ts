e127cf20c9d8b217d919c982ca2a15b1e9b19d7d
muddymudskipper <thomas.wilmering@gmail.com>
2019-09-02 16:47:34 +0100

    search field error

--- src/app/shared/showmap.component.ts
+++ src/app/shared/showmap.component.ts
@@ -1,8 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { tileLayer, latLng, marker, icon, MapOptions, Marker, Map, Control } from 'leaflet';
+//import { tileLayer, latLng, marker, icon, MapOptions, Marker, Map } from 'leaflet';
 import { VenueDetails } from '../services/types';
 import { DataService } from '../services/data.service';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+
+declare const L: any;
+import 'leaflet'
+import 'leaflet-search';
+
 
 
 
@@ -12,63 +17,77 @@ import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
   templateUrl: './showmap.component.html',
   styleUrls: ['./showmap.component.sass']
 })
+
+
+
 export class ShowMapComponent {
   
   @Input() zoom: number;
   //@Input() venues: VenueDetails[];
-  protected map: Map;
-  protected mapOptions: MapOptions;
+  protected map: L.Map;
+  protected mapOptions: L.MapOptions;
   protected venues: VenueDetails[];
-  protected layers: Marker[];
-  protected search: Control;
+  protected layers: L.Marker[];
+  //protected leafletOptions: L.ControlOptions;
+
   
 
-  constructor(private data: DataService, private sanitizer: DomSanitizer) {}
+  constructor(protected data: DataService, private sanitizer: DomSanitizer) {}
   
 
 async ngOnInit() {
-  
+    
     this.mapOptions = {
       layers: [
-        tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
           { maxZoom: 18, attribution: '...' })],
       zoom: this.zoom,
-      center: latLng(20, 20)
+      center: L.latLng(20, 20)
       
     };
 
     
-    this.venues = await this.data.getVenueCoordinates();
-    const bears = [ 'bear_blue_100.png', 'bear_blue_100a.png', 'bear_green_100.png', 
+    this.data.getVenueCoordinates().then(ven => {
+      const bears = [ 'bear_blue_100.png', 'bear_blue_100a.png', 'bear_green_100.png', 
                     'bear_green_100a.png', 'bear_orange_100.png', 'bear_orange_100a.png', 
                     'bear_pink_100.png', 'bear_pink_100a.png', 'bear_yellow_100.png',
                     'bear_yellow_100a.png' ]
 
-    var l = [];
-    this.venues.forEach(v => {
+    var l = []
+    ven.forEach(v => {
       if (v.long != undefined) {
         const venuehtml = this.venueHtml(v.shows);
         var bear = bears[Math.floor(Math.random()*bears.length)];
-        var m = marker([v.long, v.lat], {
+        var m = L.marker([v.long, v.lat], {
           riseOnHover: true,
-          icon: icon({
+          icon: L.icon({
             iconSize: [ null, 20 ],
             iconAnchor: [ 9, 19 ],
             iconUrl: 'assets/' + bear //bearmap.png',
         })
       }).bindPopup('<b> <a href="/venue/' + v.id + '">' + v.name + '</a></b>' + venuehtml);
-      l.push(m) }   
+      }   
+      l.push(m)
     });
+    
     this.layers = l;
+    
+    });
+    
 
-    //console.log(this.map);
+    
+    
 
     }
     
 
-onMapReady(map: Map) {  
+onMapReady(map: L.Map) {  
+
   this.map = map;
+  this.map.addControl( new L.Control.Search({layer: this.layers}) );
+  
 }
+
 
 venueHtml(s) {
   if (s != undefined){
