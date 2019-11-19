86be03d00f9b235f379f4bcc65724ab437c865a7
muddymudskipper <you@example.com>
2019-11-19 09:17:47 +0000

    start page work in progress

--- src/app/views/start.component.ts
+++ src/app/views/start.component.ts
@@ -9,11 +9,26 @@ import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
   templateUrl: './start.component.html'
 })
 export class StartComponent {
+  part1: String;
+  part2: String;
+  part3: String;
+  part4: String;
+  part5: String;
+  part6: String;
+  part7: String;
   
   constructor(private sanitizer: DomSanitizer,
     private router: Router, private route: ActivatedRoute) {}
- 
-  
-  
+
+  ngOnInit() {
+    this.part1 = "gdc";
+    this.part2 = "oncert";
+    this.part3 = "explo";
+    this.part4 = "rer";
+    var at = Math.pow(2,6);
+    this.part5 = String.fromCharCode(at);
+    this.part6 = "gmai";
+    this.part7 = "l.com";
+  }
 
 }
