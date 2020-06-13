cac844da52569c9a244f8b38c135a453488be1bf
muddymudskipper <thomas.wilmering@gmail.com>
2020-06-13 22:38:16 +0100

    update to Angular 8, google analytics html moved from main.ts to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { AuthService } from './auth.service';
 import { TRACKINGID, TRACKING } from './config';
-
+import { DOCUMENT } from '@angular/common';
 
 declare let gtag: Function;
 
@@ -18,7 +18,7 @@ export class AppComponent {
   //private cookieValue: string;
   protected currentUser: any = { userName: '', userId: '' };
 
-  constructor(public router:Router, public auth: AuthService) {
+  constructor(public router:Router, public auth: AuthService, @Inject(DOCUMENT) private doc: any) {
     //router.events.forEach((event) => {
     //  if (router.url.includes('/about')) {  
     //    this.start = true; 
@@ -51,7 +51,63 @@ export class AppComponent {
   }
 
   public ngOnInit() {
-    
+    this.googleAnalytics();
+  }
+
+
+  private scriptHtml(html, src){
+    const s = this.doc.createElement('script');
+    s.type = 'text/javascript';
+    if (src) s.src = src;
+    if (html) s.innerHTML = html;
+    const head = this.doc.getElementsByTagName('head')[0];
+    head.appendChild(s);
+  }
+
+
+  private googleAnalytics() {
+    this.scriptHtml(`
+      // Set to the same value as the web property used on the site
+      var gaProperty = "` + TRACKINGID + `";
+  
+      // Disable tracking if the opt-out cookie exists.
+      var disableStr = 'ga-disable-' + gaProperty;
+  
+      // if (document.cookie.indexOf(disableStr + '=true') > -1) {
+      if (document.cookie.indexOf('gd-cookieconsent=') < 0) {
+        //alert('revoke!');
+        window[disableStr] = true;
+      }
+  
+      // Opt-in function
+      function gaOptIn() {
+        var oneYearFromNow = new Date();
+        oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
+        document.cookie = 'gd-cookieconsent=allow; expires="' + oneYearFromNow.toGMTString() + '"';
+        window[disableStr] = false;
+        window.location.reload()
+      }
+  
+      // Opt-out function
+      function gaOptOut() {
+        document.cookie = 'gd-cookieconsent=; expires = Thu, 01 Jan 1970 00:00:00 GMT';
+        document.cookie = '_ga=; expires = Thu, 01 Jan 1970 00:00:00 GMT';
+        document.cookie = '_gid=; expires = Thu, 01 Jan 1970 00:00:00 GMT';
+        document.cookie = '_gat_gtag_` + TRACKINGID.replace(/-/g, '_') + `=; expires = Thu, 01 Jan 1970 00:00:00 GMT';
+        window[disableStr] = true;
+        window.location.reload()
+      }
+    `, null);
+
+    // <!-- Global site tag (gtag.js) - Google Analytics -->
+    this.scriptHtml(null, 'https://www.googletagmanager.com/gtag/js');
+    this.scriptHtml( `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    `, null)
+    this.scriptHtml(null, 'https://cdn.auth0.com/js/lock/11.23.1/lock.min.js');
+
   }
 
 
--- src/main.ts
+++ src/main.ts
@@ -2,57 +2,11 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { TRACKINGID } from './app/config';
+
 
 if (environment.production) {
   enableProdMode();
 }
-
-document.write(`
-  <script type="text/javascript">
-    // Set to the same value as the web property used on the site
-    var gaProperty = "` + TRACKINGID + `";
-
-    // Disable tracking if the opt-out cookie exists.
-    var disableStr = 'ga-disable-' + gaProperty;
-
-    // if (document.cookie.indexOf(disableStr + '=true') > -1) {
-    if (document.cookie.indexOf('gd-cookieconsent=') < 0) {
-      //alert('revoke!');
-      window[disableStr] = true;
-    }
-
-    // Opt-in function
-    function gaOptIn() {
-      var oneYearFromNow = new Date();
-      oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
-      document.cookie = 'gd-cookieconsent=allow; expires="' + oneYearFromNow.toGMTString() + '"';
-      window[disableStr] = false;
-      window.location.reload()
-    }
-
-    // Opt-out function
-    function gaOptOut() {
-      document.cookie = 'gd-cookieconsent=; expires = Thu, 01 Jan 1970 00:00:00 GMT';
-      document.cookie = '_ga=; expires = Thu, 01 Jan 1970 00:00:00 GMT';
-      document.cookie = '_gid=; expires = Thu, 01 Jan 1970 00:00:00 GMT';
-      document.cookie = '_gat_gtag_` + TRACKINGID.replace(/-/g, '_') + `=; expires = Thu, 01 Jan 1970 00:00:00 GMT';
-      window[disableStr] = true;
-      window.location.reload()
-    }
-  </script>
-
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-  </script>
-
-
-  <script src="https://cdn.auth0.com/js/lock/11.23.1/lock.min.js"></script>
-`);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
