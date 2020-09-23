76cf38d1d287d6d46fc0d5928ac03adce8a34f63
muddymudskipper <thomas.wilmering@gmail.com>
2020-09-23 14:45:58 +0100

    privacy policy updare

--- src/app/services/socketio.service.ts
+++ src/app/services/socketio.service.ts
@@ -17,8 +17,8 @@ export class SocketioService {
       //"force new connection" : true,
       //"reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
       //"timeout" : 10000,                  //before connect_error and connect_timeout are emitted.
-      //"transports" : ['polling', 'websockets'],
-      path: '/dead/socket.io',
+      //"transports" : ['websocket'],
+      //path: '/dead/socket.io',
       //secure: true
   };
 
