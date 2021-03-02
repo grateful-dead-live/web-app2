8733329c4d252ddfb7ad92d643a0ec9bb82a987c
muddymudskipper <{ID}+{username}@users.noreply.github.com>
2021-03-02 11:33:29 +0000

    update ngx-sharebuttons to 8.0.5, use DOMAIN for sharing

--- src/app/shared/sharebuttons.component.ts
+++ src/app/shared/sharebuttons.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { DOMAIN } from '../config';
 
 @Component({
   selector: 'gd-sharebuttons',
@@ -7,9 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['sharebuttons.component.sass']
 })
 export class ShareButtonsComponent {
+  public url: string;
 
-  
+  constructor(private router: Router) {}
+
   ngOnInit() {
+    this.url = 'https://' + DOMAIN + this.router.url;
   }
 
 }
